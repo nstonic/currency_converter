c348b3d4aad22a164dc4f422dc0078f008cc6019
nstonic <118763806+nstonic@users.noreply.github.com>
2023-09-14 22:40:06 +0400

    Update views.py

--- django/src/converter/views.py
+++ django/src/converter/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
+from requests import HTTPError
 from rest_framework import status
 
 from converter.services import Converter
@@ -23,7 +24,7 @@ def rate_path_view(request, from_: str, to: str, value: int):
         data = {
             "result": Converter().convert(from_, to, value)
         }
-    except ValueError as ex:
+    except (ValueError, HTTPError) as ex:
         return JsonResponse({'error': str(ex)}, status=status.HTTP_400_BAD_REQUEST)
 
     return JsonResponse(data, status=status.HTTP_200_OK)
